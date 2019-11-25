d53d8f5cf7a92ac5becb0ab63b794052ee8e0853
cferrero <cferrero@CFERRERO-AR.ar.oracle.com>
2019-11-25 17:34:42 -0300

    git ignore

--- source/server/app/initializers/02_mongodb.js
+++ source/server/app/initializers/02_mongodb.js
@@ -7,5 +7,10 @@
 //Require de la libreria JS mongoose.
 var mongoose = require('mongoose');
 
+//Configs
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
 //Se conecta a la base de datos.
 module.exports = gamedb = mongoose.createConnection( config.data_base.url );
